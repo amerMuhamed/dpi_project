a470808ebc68eec09a2f35882a4690a7b916661f
amer <amermuhamedamer@gmail.com>
2024-09-25 02:14:09 +0300

    DEPI Graduation Project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "#0064D2",
+        secondaryColor: "#ffa500",
+        thirdColor: "#FCBD01",
+        orangeJP: "#F1663A",
+        background: "#FAF8F7",
+        background2: "#4c3b4f5c",
+
+      },
+    },
+  },
+  plugins: [],
+};
